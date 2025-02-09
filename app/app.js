69abf920eb0834559b81815f58e3d6d95e264081
Regy <dev@regyeduardo.com>
2025-02-09 16:44:23 -0300

    fix: containmer dependency

--- app/app.js
+++ app/app.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const app = express();
 const port = 3000;
-const waitOn = require('wait-on');
 
 const mysql = require("mysql2");
 const connection = mysql.createConnection({
@@ -11,24 +10,19 @@ const connection = mysql.createConnection({
   database: "main",
 });
 
-const opts = {
-  resources: [
-    'db:3306',
-  ],
-};
-
 app.use(express.json());
 
-waitOn(opts, function (err) {
-  if (err) {
-    return handleError(err);
-  }
+connection.connect(function (err) {
+  if (err) throw err;
+  console.log("Connected!");
 
-  connection.connect(function (err) {
+  var sql = "INSERT INTO pessoa (nome) VALUES ('Rafael'), ('Renata'), ('Cássia');"
+
+  connection.query(sql, function (err, result) {
     if (err) throw err;
-    console.log("Connected!");
   });
 });
+
 
 app.get("/", (req, res) => {
   let data = "Full Cycle Rocks!</h1>\n<br>";
@@ -45,17 +39,17 @@ app.get("/", (req, res) => {
   });
 });
 
-app.post("/", (req, res) => {
-  const { nome } = req.body
+// app.post("/", (req, res) => {
+//   const { nome } = req.body
 
-  if (!nome) res.status(400).send("Must insert a name")
-  query = `INSERT INTO pessoa (nome) values (?)`;
+//   if (!nome) res.status(400).send("Must insert a name")
+//   query = `INSERT INTO pessoa (nome) values (?)`;
 
-  connection.query(query, [nome], (err) => {
-    if (err) throw err;
-  });
-  res.status(201).send();
-});
+//   connection.query(query, [nome], (err) => {
+//     if (err) throw err;
+//   });
+//   res.status(201).send();
+// });
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
